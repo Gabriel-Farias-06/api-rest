cf002e991730d7192050eb53f6da1b9031770250
Gabriel do Rego Farias <gabriel.rfarias@a.ucb.br>
2025-04-16 00:00:39 -0300

    Update ApiRestApplication.java

--- src/main/java/me/dio/ApiRestApplication.java
+++ src/main/java/me/dio/ApiRestApplication.java
@@ -1,7 +1,8 @@
 package me.dio;
 
 import io.swagger.v3.oas.annotations.servers.Server;
-import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server")})
