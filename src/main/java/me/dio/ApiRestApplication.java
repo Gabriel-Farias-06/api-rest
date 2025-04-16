688f4a29db3082e1414e81a2a6e160102cd871b6
Gabriel do Rego Farias <gabriel.rfarias@a.ucb.br>
2025-04-15 23:41:20 -0300

    Correção no CORS

--- src/main/java/me/dio/ApiRestApplication.java
+++ src/main/java/me/dio/ApiRestApplication.java
@@ -1,8 +1,11 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.*;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server")})
 @SpringBootApplication
 public class ApiRestApplication {
 
