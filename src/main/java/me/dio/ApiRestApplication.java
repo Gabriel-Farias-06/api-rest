67ff149b54ad6d947816a261799fbd51cfc15766
Gabriel do Rego Farias <gabriel.rfarias@a.ucb.br>
2025-05-23 21:05:29 -0300

    Tela inicial api

--- src/main/java/me/dio/ApiRestApplication.java
+++ src/main/java/me/dio/ApiRestApplication.java
@@ -2,7 +2,7 @@ package me.dio;
 
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server")})
---
+++ src/main/java/me/dio/controller/HomeController.java
@@ -0,0 +1,13 @@
+package me.dio.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public ResponseEntity<String> inicialPage() {
+        return ResponseEntity.ok("API REST Iniciada com Sucesso!");
+    }
+}
